2811c7d452da53403d5f1571ef7d2ed4fb28132e
kabdurahmonov <abdurahmonovk392@gmail.com>
2026-02-10 23:18:00 +0500

    initial commit

---
+++ Onbirinchidars/School.cs
@@ -0,0 +1,11 @@
+﻿namespace Onbirinchidars
+{
+    internal class School
+    {
+        public int Number { get; set; }
+        public string? Name { get; set; }   
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public int CurrentStudentCount { get; set; }
+    }
+}
